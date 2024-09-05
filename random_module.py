d3cb3a1fb00d2d9556a8145cc0f0f77faa2df300
santhosh-techi <123259331+santhosh-techi@users.noreply.github.com>
2024-09-05 23:36:51 +0530

    Delete random_module.py

--- random_module.py
+++
@@ -1,7 +0,0 @@
-import random as r
-
-print(r.randrange(4)) #it will give random value only positive int values
-print(r.randint(1,5))  #it will generate random values between 1 and 5, only int values
-print(r.random())  # it will generate randon fractional values
-print(r.randbytes(4))  #generates random aplha values
-print(r.randrange(1,5))
