8f458aacd92e16ca573aa7e65ed44bd38756cb33
santhosh-techi <123259331+santhosh-techi@users.noreply.github.com>
2024-10-29 15:33:19 +0530

    Delete Matrix.py

--- Matrix.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-array_2d=[[10,20,30],[40,50,60],[70,80,90]]
-print(array_2d)
-
-#converting the list into array
-matrix=np.array(array_2d)  #converting the list elements into array elements
-
-res=matrix[1:3,0:2]  #slicing methods
-print(res)
